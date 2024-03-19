0521ddcc36facece05f68df5d2c3da6debec7663
K Ramkrishna Achary <kramkrishnaachary@H1FDVTX544.local>
2024-03-19 13:33:40 +0530

    Added calc py file for testing django simple test framework

---
+++ app/app/calc.py
@@ -0,0 +1,7 @@
+"""
+Calculator Function
+"""
+
+def add(x,y):
+    """ Add x and y return result """
+    return x+y
---
+++ app/app/tests.py
@@ -0,0 +1,17 @@
+"""
+Sample Tests
+"""
+
+from django.test import SimpleTestCase
+from app import calc
+
+
+class CalsTests(SimpleTestCase):
+    """Test the calc module"""
+
+    def test_add_numbers(self):
+        """Test adding numbers together"""
+        res = calc.add(5,6)
+
+        """Validating the result"""
+        self.assertEqual(res, 11)
