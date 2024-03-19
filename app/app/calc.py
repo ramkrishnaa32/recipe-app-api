73fbdf9443b33701f02b24a0eb19d5dad7e5facd
K Ramkrishna Achary <kramkrishnaachary@H1FDVTX544.local>
2024-03-19 16:41:31 +0530

    Whitespace fix

--- app/app/calc.py
+++ app/app/calc.py
@@ -3,9 +3,11 @@ Calculator Function
 """
 
 def add(x, y):
+    
     """Add x and y return result"""
     return x + y
 
 def substract(x, y):
+
     """Substract x and y return result"""
     return x - y
--- app/app/tests.py
+++ app/app/tests.py
@@ -10,17 +10,17 @@ class CalsTests(SimpleTestCase):
     """Test the calc module"""
 
     def test_add_numbers(self):
+
         """Test adding numbers together"""
         res = calc.add(5, 6)
 
         """Validating the result"""
         self.assertEqual(res, 11)
 
-
     def test_substract_numbers(self):
+
         """Test substract numbers together"""
         res = calc.substract(15, 5)
 
         """Validating the result"""
         self.assertEqual(res, 10)
-     
